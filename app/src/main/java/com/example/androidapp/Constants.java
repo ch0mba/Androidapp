1bc681e1c251980fa1c89a2be9c1402bc672fff5
ch0mba <chombaerickdickson@gmail.com>
2024-10-12 12:42:35 +0300

    first_commit

---
+++ app/src/main/java/com/example/androidapp/Constants.java
@@ -0,0 +1,6 @@
+package com.example.androidapp;
+
+public class Constants {
+    public static final String ROOT_URL =  "http://192.168.0.188:4433/AndroidApp/webservices/";
+    public static final String URL_REGISTER = ROOT_URL+"register_user.php";
+}
