8321d7afd9edd9863d7a0c11dc8fd59869cc359f
ch0mba <chombaerickdickson@gmail.com>
2024-10-15 16:15:17 +0300

    second_commit

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.androidapp;
 
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -12,11 +12,9 @@ import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,7 +24,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText editTextUsername, editTextPassword;
+    private EditText editTextUsername, editTextPassword, editTextFirstname, editTextLastname;
     private Button buttonRegister;
     private ProgressDialog progressDialog;
 
@@ -36,7 +34,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         editTextUsername = findViewById(R.id.editTextUsername);
+        editTextFirstname = findViewById(R.id.editTextFirstname);
+        editTextLastname = findViewById(R.id.editTextLastname);
         editTextPassword = findViewById(R.id.editTextPassword);
+
 
         buttonRegister =(Button) findViewById(R.id.buttonRegister);
 
@@ -48,6 +49,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     private void registerUser(){
         final String username =editTextUsername.getText().toString().trim();
+        final String first_name =editTextFirstname.getText().toString().trim();
+        final String last_name =editTextLastname.getText().toString().trim();
         final String password =editTextPassword.getText().toString().trim();
 
         progressDialog.setMessage("Registering user...");
@@ -82,12 +85,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String,String> params = new HashMap<>();
                 params.put("username", username);
+                params.put("first_name", first_name);
+                params.put("last_name", last_name);
                 params.put("password", password);
                 return params;
             }
         };
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
     }
 
     @Override
